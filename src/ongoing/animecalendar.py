dce03f07bc7c2a80eeb59fd1f3cae3199201617b
Ilya Petrov <ilya.muromec@gmail.com>
2011-04-25 01:27:28 +0300

    parse disabled/enabled

--- src/ongoing/animecalendar.py
+++ src/ongoing/animecalendar.py
@@ -6,7 +6,8 @@ from google.appengine.ext import deferred
 from tipfy import Response, RequestHandler
 from models import Title
 
-title_re = re.compile(r'<a href="/show/[0-9]+/.*">(.*)</a>')
+title_re = re.compile(r'(disable">|) <a href="/show/[0-9]+/.*">(.*)</a>') 
+
 
 class Import(RequestHandler):
   def get(self):
@@ -14,18 +15,20 @@ class Import(RequestHandler):
     remote = urllib.urlopen('http://www.animecalendar.net/shows/list/all')
 
     titles = re.findall(title_re, remote.read())
-    titles = [t.decode('utf8') for t in titles]
+    titles = [ (not d,t.decode('utf8')) for d,t in titles]
     deferred.defer(titles_update, titles)
 
     return Response("nom-nom-nom")
 
 def titles_update(titles):
-  t = titles.pop()
+  mode,t = titles.pop()
+  import logging
+  logging.info("mode: %r, title %r" % (mode, t))
 
   title_db = Title.get_by_key_name(t)
 
   if title_db:
-    title_db.show = True
+    title_db.show = mode
     title_db.put()
 
   if not titles:
